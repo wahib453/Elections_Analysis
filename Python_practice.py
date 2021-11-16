a6d4d5a3c96dabb7864a73c841a9be7aa14c0cbd
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-16 00:15:41 -0600

    create Python_practice.py file for practice of the decision statements

---
+++ Python_practice.py
@@ -0,0 +1,9 @@
+print("Hello world")
+Add = 5 + 2 * 3
+print(Add)
+
+# Determine if the second counties in the list is Denver. If it's, print to the screen.
+counties = ['Arapahoes', 'Denver', 'Jefferson']
+if counties[1] == 'Denver':
+    print(counties[1])
+    
