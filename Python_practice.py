e45c39b8cacc0a0c2ab774726d449e5e8dc3f73e
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-16 12:31:13 -0600

    update the Python_practice.py file

--- Python_practice.py
+++ Python_practice.py
@@ -1,3 +1,6 @@
+from typing_extensions import IntVar
+
+
 print("Hello world")
 Add = 5 + 2 * 3
 print(Add)
@@ -16,3 +19,41 @@ if temperature > 80: # the temperature is in degree Fahrenheit.
 
 else:
     print('Open the window.')
+
+# Determine your grade using if-else statement
+score = int(input('What is your grade?'))
+if score >= 90:
+    print('your grade is an A.')
+
+else:
+    if score >= 80:
+        print("Your grade is a B.")
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is F.') 
+                
+# Rewrite the above statement using if-elif-else
+score = int(input('What is your grade?'))
+if score >= 90:
+    print('your grade is an A.')
+
+elif score >= 80:
+    print("Your grade is a B.")
+
+elif score >= 70:
+    print('Your grade is a C.')
+        
+elif score >= 60:
+    print('Your grade is a D.')
+            
+else:
+    print('Your grade is F.')
+
+    
+    
+    
