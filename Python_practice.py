6bb7ec4608228e4d88d49408f4eb898112f308a5
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-16 23:39:09 -0600

    update the Python_practice.py file

--- Python_practice.py
+++ Python_practice.py
@@ -36,7 +36,7 @@ else:
                 print('Your grade is a D.')
             else:
                 print('Your grade is F.') 
-                
+
 # Rewrite the above statement using if-elif-else
 score = int(input('What is your grade?'))
 if score >= 90:
@@ -54,6 +54,31 @@ elif score >= 60:
 else:
     print('Your grade is F.')
 
+# Determine if El Paso is in the counties list.
+counties = ['Arapahoes', 'Denver', 'Jefferson']
+if 'El Paso' in counties:
+    print('El Paso is in the list of counties.')
+else:
+    print('El Paso is not in the list of counties.')
     
-    
-    
+# Practice on logical operators: 'and', 'or', 'not'
+# practice on logical operator: and
+x = 5 
+y = 5    
+
+if x == 5 and y == 5:
+    print('turn')
+else:
+    print('false')
+
+# practice on logical operator: or
+if x == 3 or y == 5:
+    print('true')    
+else:
+    print('false')
+
+# practice on logical operator: not
+if not (x > y):
+    print('true')
+else:
+    print('false')
