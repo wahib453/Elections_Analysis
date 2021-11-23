e579b0fe9dd6f526119ac6862c6801f8c2335322
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-22 22:01:57 -0600

    update PyPoll.py  file

--- PyPoll.py
+++ PyPoll.py
@@ -1,6 +1,44 @@
-# The data we need to retrieve.
-# 1. the total number of votes cast
-# 2. a complete list of candidates who received votes
-# 3. the percentage of votes each candidate won
-# 4. the total number of votes each candidate won
-# 5. the winner of the election based on popular vote.
+##################################################################################################################################################################################################################################################
+# Section 3.4.2: Open and Read Files using Python
+# Open and close a file using the open() and close()
+# assign a variable for the file to load and the path.
+file_to_load = 'Resources\election_results.csv'
+
+# open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+
+# Close the file.
+election_data.close()
+
+# Alternatively, open and close a file using with() statement
+# Open the election results and read the file
+file_to_load = '.\Resources\election_results.csv'
+
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+     
+# Indirect way of accessing a file
+import csv
+import os
+
+file_to_load = os.path.join('Resources', 'election_results.csv')
+
+# open the election results and read the file.
+with open(file_to_load) as election_data:
+     # Print the file
+     print(election_data)
+
+##########################################################################################################################################################################################################################################
+# Section 3.4.3: Write to Files with Python
+
+import csv
+import os
+# create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join('Analysis', 'election_analysis.txt')
+# using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, 'w')
