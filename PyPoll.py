eecfaee8bc7fc8c0b0448fe0afa84c4cfb65c7eb
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-24 00:27:28 -0600

    add updated pypoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -76,6 +76,10 @@ file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
 # 1. Add the total vote counter before the with open() statement and initialize it with zero.
 total_votes = 0
+
+# Candidate options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -84,11 +88,25 @@ with open(file_to_load) as election_data:
     
     # print the header row.
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
 
     # print each row in the csv file.
     for row in file_reader:
+         # print(row)
          # 2. Add to the total vote count.
          total_votes += 1
+         # Print the candidate name from each row.
+         candidate_name = row[2]
+         # If the candidate does not match any existing candidate...
+         if candidate_name not in candidate_options:
+              # Add it to the list of candidates.
+              candidate_options.append(candidate_name)
+
 # 3. print the total votes
 print(total_votes)
+# Print the candidate options
+print(candidate_options)
+
+
+##############################################################################################################################################################################################################################################
+# Section 3.5.2: Get the candidats in the Election
