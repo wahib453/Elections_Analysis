fe30bd08ef2a9655fa806885fadb97e0c7dd7dcf
Ibrahim Wahab <Sa2diqw@DESKTOP-UN2PGC0>
2021-11-25 15:27:36 -0600

    Add additional info to PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -111,15 +111,15 @@ with open(file_to_load) as election_data:
          # Add a vote to that candidate's count. 
          candidate_votes[candidate_name] += 1
          
-         # Determine the percentage of votes for each candidate by looping through the counts.
-         # # 1. Iterate through the candidate list.
-         for candidate_name in candidate_votes:
-              # 2. Retrieve vote count of a candidate.
-              votes = candidate_votes[candidate_name]
-              # 3. Calculate the percentage of votes.
-              vote_percentage = float(votes) / float(total_votes) * 100
-              # 4. Print the candidate name and percentage of votes.
-              #print(f"{candidate_name}: received {float(votes) / float(total_votes) * 100:.1f}% of the vote.")
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+     # Retrieve vote count of a candidate.
+     votes = candidate_votes[candidate_name]
+     # Calculate the percentage of votes.
+     vote_percentage = float(votes) / float(total_votes) * 100
+     # Print the candidate name and percentage of votes.
+     print(f"{candidate_name}: received {float(votes) / float(total_votes) * 100:.1f}% of the vote.")
        
 # 3. print the total votes
 print(total_votes)
@@ -130,3 +130,5 @@ print(candidate_options)
 # Print the candidate vote dictionary.
 print(candidate_votes)
 
+# Print the candidate name and percentage of votes.
+#print(f"{candidate_name}: received {float(votes) / float(total_votes) * 100:.1f}% of the vote.")
